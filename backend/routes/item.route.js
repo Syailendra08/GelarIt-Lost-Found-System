661b1aa99a8b97ec2bc2121ecefd64ee018b3a4e
Syailendra08 <syailendraanggoromukti@smkwikrama.ch.id>
2026-05-19 22:13:42 +0700

    change: delete add found

--- backend/routes/item.route.js
+++ backend/routes/item.route.js
@@ -4,10 +4,8 @@ const itemController = require("../controllers/item.controller");
 const upload = require("../middlewares/upload");
 
 router.post(
-    "/lost", upload.single('image'), itemController.createItem
+    "/", upload.single('image'), itemController.createItem
 );
-
-router.post("/found", upload.single('image'), itemController.addFoundItem);
 
 router.get(
     "/",
@@ -15,7 +13,7 @@ router.get(
 );
 
 router.get("/:id", itemController.showItem)
-router.put("/:id", upload.single('image'), itemController.updateItem);
+router.put("/:id", upload.single('image'), itemController.updateItem)
 router.delete("/:id", itemController.deleteItem)
 router.patch("/restore/:id",  itemController.restoreItem);
 
