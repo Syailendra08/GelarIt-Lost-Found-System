3ecb758fd049fa1e75bdc0f57ce882eaec425204
Syailendra08 <syailendraanggoromukti@smkwikrama.ch.id>
2026-05-30 16:42:56 +0700

    feat: Item Status Chart

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,9 +1,54 @@
-import DashboardStats from "../../components/Admin/DashboardStats";
+import { useEffect, useState } from "react";
 
-export default function AdminDashboard() {
+import DashboardStats from "../../components/Admin/DashboardStats";
+import { getItemStats } from "../../api/item.api";
+import ItemStatusChart from "../../components/Admin/ItemStatusChart";
+
+export default function Dashboard() {
+
+    const [stats, setStats] = useState(null);
+
+  
+
+    const fetchItemStats = async () => {
+        try {
+            const response = await getItemStats();
+
+            setStats(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        fetchItemStats();
+    }, []);
+
+    if (!stats) return <p>Loading...</p>;
+
+    const chartData = [
+        {
+            name: "Lost",
+            value: stats.lost
+        },
+        {
+            name: "Found",
+            value: stats.found
+        },
+        {
+            name: "Claimed",
+            value: stats.claimed
+        },
+        {
+            name: "Taken",
+            value: stats.taken
+        }
+    ];
+
+      
     return (
         <>
         <DashboardStats />
+      <ItemStatusChart data={chartData} />
         </>
     )
 }
